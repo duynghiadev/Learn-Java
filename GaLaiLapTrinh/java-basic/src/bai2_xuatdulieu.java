4ef02ddb486a5e19ebbe0f9f5d9ad6db4c76e337
duynghiadev <duynghia22302@gmail.com>
2023-12-16 01:02:45 +0700

    1.Tự học java siêu tốc dễ hiểu cho người mới Cài đặt intellij idea | java 2023 tutorial

---
+++ GaLaiLapTrinh/java-basic/src/bai2_xuatdulieu.java
@@ -0,0 +1,19 @@
+public class bai2_xuatdulieu {
+  public static void main(String[] args) {
+    /**
+     * - dòng này in ra và xuống hàng. Do có sử dụng println
+     * - Tóm lại thì khi kết thúc câu lệnh thì con trỏ chuột nó sẽ xuống hàng, và in ra từng hàng và từng hàng đó sẽ xuống hàng
+     */
+    System.out.println("dòng 1");
+    System.out.println("dòng 2");
+    System.out.println("dòng 3");
+
+    /**
+     * - dòng này in ra và không xuống hàng -> nằm trên 1 hàng. Do có sử dụng print
+     * - Tóm lại thì khi kết thúc câu lệnh thì con trỏ chuột nó sẽ không xuống hàng, mà trỏ đến cuối hàng và ghi chữ tiếp theo vào đó
+     */
+    System.out.print("dòng 4");
+    System.out.print("dòng 5");
+    System.out.print("dòng 6");
+  }
+}
--- GaLaiLapTrinh/java/README.md
+++
@@ -1,3 +0,0 @@
-- Đây là khóa học Lập trình java | Tự học java siêu tốc | Java cho người mới từ con số 0 | Java 2023 tutorial full course
-
-[Link Tutorial](https://bom.so/zbHyu7)
