5dabfc9ef6c706128ace175b9dec2b7f97da3904
duynghiadev <duynghia22302@gmail.com>
2023-12-16 01:52:35 +0700

    4. Kiểu dữ liệu trong java - java cơ bản cho người mới học

---
+++ GaLaiLapTrinh/java-basic/src/bai4_kieudulieutrongjava/bai4_kieudulieutrongjava.java
@@ -0,0 +1,11 @@
+public class bai4_kieudulieutrongjava {
+  public static void main(String[] args) {
+    // kiểm tra kiểu nguyên
+    System.out.println("max của kiểu int: " + Integer.MAX_VALUE);
+    System.out.println("min của kiểu int: " + Integer.MIN_VALUE);
+
+    // kiểm tra kiểu byte
+    System.out.println("max của kiểu byte: " + Byte.MAX_VALUE);
+    System.out.println("min của kiểu byte " + Byte.MIN_VALUE);
+  }
+}
