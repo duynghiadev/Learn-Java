667b89a50056690e2d7c61da985ed7c93c931671
duynghiadev <duynghia22302@gmail.com>
2023-12-16 01:22:02 +0700

    2. Hướng dẫn nhập và xuất dữ liệu java trong 5 phút - chi tiết dễ hiểu cho người mới

--- GaLaiLapTrinh/java-basic/Readme.md
+++ GaLaiLapTrinh/java-basic/Readme.md
@@ -1,3 +1,4 @@
-- Đây là khóa học Lập trình java | Tự học java siêu tốc | Java cho người mới từ con số 0 | Java 2023 tutorial full course
+- Đây là khóa học Lập trình java | Tự học java siêu tốc | Java cho người mới từ con số 0 | Java 2023 tutorial
+  full course
 
-[Link Tutorial](https://bom.so/zbHyu7)
+[Link Tutorial](https://bom.so/zbHyu7) 
--- GaLaiLapTrinh/java-basic/src/bai2_xuatdulieu.java
+++ GaLaiLapTrinh/java-basic/src/bai2_xuatdulieu.java
@@ -2,7 +2,8 @@ public class bai2_xuatdulieu {
   public static void main(String[] args) {
     /**
      * - dòng này in ra và xuống hàng. Do có sử dụng println
-     * - Tóm lại thì khi kết thúc câu lệnh thì con trỏ chuột nó sẽ xuống hàng, và in ra từng hàng và từng hàng đó sẽ xuống hàng
+     * - Tóm lại thì khi kết thúc câu lệnh thì con trỏ chuột nó sẽ xuống hàng, và in ra từng hàng, và mỗi
+     * dòng in đó nó sẽ xuống hàng
      */
     System.out.println("dòng 1");
     System.out.println("dòng 2");
@@ -10,10 +11,22 @@ public class bai2_xuatdulieu {
 
     /**
      * - dòng này in ra và không xuống hàng -> nằm trên 1 hàng. Do có sử dụng print
-     * - Tóm lại thì khi kết thúc câu lệnh thì con trỏ chuột nó sẽ không xuống hàng, mà trỏ đến cuối hàng và ghi chữ tiếp theo vào đó
+     * - Tóm lại thì khi kết thúc câu lệnh thì con trỏ chuột nó sẽ không xuống hàng, mà trỏ đến cuối hàng
+     * và ghi chữ tiếp theo vào đó
      */
     System.out.print("dòng 4");
     System.out.print("dòng 5");
     System.out.print("dòng 6");
+    System.out.println("\n");
+
+    // Ví dụ về \t
+    System.out.println("Cộng hòa xã hội chủ nghĩa Việt Nam");
+    System.out.println("\tĐộc lập - Tự do - Hạnh Phúc");
+
+    // \n
+    System.out.println("Sông cầu nước chảy lơ thơ\nCó đôi trai gái ngồi hơ quần đùi");
+
+    // dấu nháy "
+    System.out.println("Có người nói rằng \"Cho đi là còn mãi\"");
   }
 }
