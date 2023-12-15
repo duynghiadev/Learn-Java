8a66bc96d7991823489a592cc10ca4e909477048
duynghiadev <duynghia22302@gmail.com>
2023-12-16 00:21:44 +0700

    1.Tự học java siêu tốc dễ hiểu cho người mới Cài đặt intellij idea | java 2023 tutorial

---
+++ GaLaiLapTrinh/java-basic/src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Mình là Duy Nghĩa Đây nè mọi người");
+        System.out.println(123456);
+    }
+}
