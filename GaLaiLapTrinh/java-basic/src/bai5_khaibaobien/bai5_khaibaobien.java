bad7622f7e4901e03eb26cad897dcedee2cc8e17
duynghiadev <duynghia22302@gmail.com>
2023-12-16 02:13:30 +0700

    5. Khai báo biến trong java - hướng dẫn nhanh và dễ hiểu biến java trong 8 phút

---
+++ GaLaiLapTrinh/java-basic/src/bai5_khaibaobien/bai5_khaibaobien.java
@@ -0,0 +1,24 @@
+package bai5_khaibaobien;
+
+public class bai5_khaibaobien {
+  public static void main(String[] args) {
+    // khai báo biến
+    int a;
+    byte tuoi;
+    float diemToan;
+    // khai báo nhiều biến 1 lúc cùng kiểu
+    int c, d, e, f;
+
+    // khởi tạo biến
+    int tuoiCon = 30;
+    float diemVan = 7.5f;
+    double dienAnh = 8.9;
+
+    System.out.println("tuoiCon trước thay đổi: " + tuoiCon);
+    tuoiCon = 15;
+    System.out.println("tuoiCon sau thay đổi: " + tuoiCon);
+
+    // hằng số
+    final int DOSOI = 100;
+  }
+}
