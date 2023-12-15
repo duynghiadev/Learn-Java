8d821c11dd9f3dd6d4531c1bdc3d852f2a5684a7
duynghiadev <duynghia22302@gmail.com>
2023-12-16 01:30:35 +0700

    3. Ghi chú trong java - tự học lập trình java cơ bản new tutorial

---
+++ GaLaiLapTrinh/java-basic/src/bai3_ghichu.java
@@ -0,0 +1,12 @@
+public class bai3_ghichu {
+  public static void main(String[] args) {
+    // Đây là ghi chú 1 dòng
+    System.out.println("Dòng 1");
+    System.out.println("Dòng 2");
+    System.out.println("Dòng 3");
+
+    /**
+     * Đây là ghi chú nhiều dòng
+     */
+  }
+}
