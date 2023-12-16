915d46a9783c994bf2ffe6e75544c4f492c5ee36
duynghiadev <duynghia22302@gmail.com>
2023-12-16 20:59:50 +0700

    #5. Variables (Khái niệm biến) | Khóa Học Java Cơ Bản Từ A tới Z cho Beginner

--- HoiDanIT/java/src/HelloWorld.java
+++ HoiDanIT/java/src/HelloWorld.java
@@ -1,5 +1,15 @@
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("HelloWorld");
-    }
+  public static void main(String[] args) {
+    int a = 6;
+    int b = 9;
+    int c = a + b;
+    System.out.println("Sum: " + c);
+
+    boolean d = true;
+    d = false;
+    System.out.println("Giá trị của biến d là: " + d);
+
+    int test = 5 + 9 * 3 - 10 / 2;
+    System.out.println("Result test is: " + test);
+  }
 }
