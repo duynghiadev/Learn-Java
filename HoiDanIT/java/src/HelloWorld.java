12a860a67618405ba2973a0cd399f16b4e92937b
duynghiadev <duynghia22302@gmail.com>
2023-12-19 04:05:33 +0700

    #7. Các hàm toán học | Khóa Học Java Cơ Bản Từ A tới Z cho Beginner

--- HoiDanIT/java/src/HelloWorld.java
+++ HoiDanIT/java/src/HelloWorld.java
@@ -2,12 +2,22 @@ import java.util.Scanner;
 
 public class HelloWorld {
   public static void main(String[] args) {
+    int a = 6;
+    int b = 9;
+    int c = Math.max(a, b);
+    System.out.println("Max = " + c);
+    int d = Math.min(a, b);
+    System.out.println("Min = " + d);
+
     Scanner scanner = new Scanner(System.in);
-    System.out.println("Your name is: ");
-    String name = scanner.nextLine();
-    System.out.println("Your age is: ");
-    int age = scanner.nextInt();
-    System.out.println("My name is " + name + ", age " + age);
+    System.out.println("First number: " + Math.pow(2, 3)); // 2 mũ 3
+    int firstNumber = scanner.nextInt();
+
+    System.out.println("Second number: " + Math.sqrt(49)); // căn bậc 2
+    int secondNumber = scanner.nextInt();
+
+    System.out.println("Min =  " + Math.min(firstNumber, secondNumber));
+    System.out.println("Max =  " + Math.max(firstNumber, secondNumber));
     scanner.close();
   }
 }
