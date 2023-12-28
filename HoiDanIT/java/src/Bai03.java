f8d62dfeb7ce9dd3e17cfd6ff75741e12f4b166a
duynghiadev <duynghia22302@gmail.com>
2023-12-28 11:38:27 +0700

    #12. Java If...Else | Khóa Học Java Cơ Bản Từ A tới Z cho Beginner

--- HoiDanIT/java/src/Bai03.java
+++ HoiDanIT/java/src/Bai03.java
@@ -2,11 +2,24 @@ import java.util.Scanner;
 
 public class Bai03 {
   public static void main(String[] args) {
-    System.out.println("Nhập vào cạnh hình lập phương: ");
+    /**
+     * - Dưới 10M: không đóng thuế
+     * - Từ 10M đến 15M: thuế 10%
+     * - Từ 15M đến 30M: thuế 20%
+     * - Trên 30M: thuế 50%
+     */
     Scanner scanner = new Scanner(System.in);
-    int canh = scanner.nextInt();
-    double theTich = Math.pow(canh, 3);
-    System.out.println("Thể tích = " + theTich);
-    scanner.close();
+    System.out.println("tax = ");
+    int tax = scanner.nextInt();
+
+    if (tax < 10) {
+      System.out.println("No tax");
+    } else if (10 <= tax && tax <= 15) {
+      System.out.println("tax 10%");
+    } else if (15 <= tax && tax <= 30) {
+      System.out.println("tax 20%");
+    } else {
+      System.out.println("tax 50%");
+    }
   }
 }
