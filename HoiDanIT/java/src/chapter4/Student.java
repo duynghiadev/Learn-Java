0a521ef40edee4554bebd7e20e3a332b8e21cbf4
duynghiadev <duynghia22302@gmail.com>
2024-01-05 15:19:05 +0700

    #23. Thực hành tạo Class/Object | Khóa Học Java Cơ Bản Từ A tới Z cho Beginner

---
+++ HoiDanIT/java/src/chapter4/Student.java
@@ -0,0 +1,10 @@
+package chapter4;
+
+public class Student {
+  String name;
+  int age;
+
+  public void learnJava() {
+    System.out.println("Learn Java with youtube Hỏi Dân IT");
+  }
+}
---
+++ HoiDanIT/java/src/chapter4/Video23.java
@@ -0,0 +1,14 @@
+package chapter4;
+
+public class Video23 {
+  public static void main(String[] args) {
+    System.out.println("Run video 23");
+
+    // Khai báo 1 object và gán giá trị
+    Student st1 = new Student();
+    st1.name = "Duynghiadev";
+    st1.age = 22;
+
+    System.out.println("student with name = " + st1.name + " and age = " + st1.age);
+  }
+}
