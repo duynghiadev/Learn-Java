999a313c98173ee5b67ae06b32c60a35b72e3cea
duynghiadev <duynghia22302@gmail.com>
2024-01-05 16:55:53 +0700

    11. câu lệnh if else java - bài giảng java 2023 - bài tập java 04-09

--- GaLaiLapTrinh/java-basic/src/bai11_ifelse/LamBaiTap/bai07.java
+++ GaLaiLapTrinh/java-basic/src/bai11_ifelse/LamBaiTap/bai07.java
@@ -7,6 +7,7 @@ public class bai07 {
     Scanner sc = new Scanner(System.in);
     System.out.println("Nhập tháng cần tra cứu: ");
     double thang = sc.nextDouble();
+    
     if ((thang == 1) || (thang == 3) || (thang == 5) || (thang == 7) || (thang == 8) || (thang == 10) || (thang == 12)) {
       System.out.println("Tháng này có 31 ngày");
     } else if ((thang == 4) || (thang == 6) || (thang == 9) || (thang == 11)) {
