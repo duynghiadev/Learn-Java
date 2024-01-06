33f2cccf3967eca705eb3ade1d7f19df106739bf
duynghiadev <duynghia22302@gmail.com>
2024-01-06 21:06:29 +0700

    #25. Class Method (Phương thức của class) | Khóa Học Java Cơ Bản Từ A tới Z cho Beginner

--- HoiDanIT/java/src/chapter4/Student.java
+++ HoiDanIT/java/src/chapter4/Student.java
@@ -1,13 +1,19 @@
 package chapter4;
 
 public class Student {
+  // class attribute
   String name;
   int age;
   String fullname;
   String firstName;
   String emailAddress;
 
-  public void learnJava() {
+  // class method
+  void learnJava() {
     System.out.println("Learn Java with youtube Hỏi Dân IT");
+  }
+
+  String getAge() {
+    return "Duy Nghia Dev";
   }
 }
