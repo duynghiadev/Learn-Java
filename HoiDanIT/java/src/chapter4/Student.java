58c98f7885822d0d860f43e07827e269f5d51503
duynghiadev <duynghia22302@gmail.com>
2024-01-06 20:56:15 +0700

    #24. Class Attributes (Thuộc tính của Class) | Khóa Học Java Cơ Bản Từ A tới Z cho Beginner

--- HoiDanIT/java/src/chapter4/Student.java
+++ HoiDanIT/java/src/chapter4/Student.java
@@ -3,6 +3,9 @@ package chapter4;
 public class Student {
   String name;
   int age;
+  String fullname;
+  String firstName;
+  String emailAddress;
 
   public void learnJava() {
     System.out.println("Learn Java with youtube Hỏi Dân IT");
