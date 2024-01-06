adc93194593353bb368557d6986e6a3b6ece5f81
duynghiadev <duynghia22302@gmail.com>
2024-01-06 18:43:23 +0700

    12. Toán tử 3 ngôi java - bài tập java 10 - seri tự học java 2023 cho người mới bắt đầu

--- GaLaiLapTrinh/java-basic/src/bai12_toantu3ngoi/baitap10.java
+++ GaLaiLapTrinh/java-basic/src/bai12_toantu3ngoi/baitap10.java
@@ -9,9 +9,9 @@ public class baitap10 {
     double avgScore;
 
     System.out.println("Vui lòng nhập vào điểm trung bình của bạn: ");
-    avgScore = input.nextDouble(); // Nhap DTB
+    avgScore = input.nextDouble(); // Nhập Điểm Trung Bình
 
-    // Kiem tra ket qua bang toan tu 3 ngoi
+    // Kiểm tra toán tử bằng toán tử 3 ngôi
     String resCheck = (avgScore <= 10 && avgScore >= 8) ? "Giỏi" :
         ((avgScore < 8 && avgScore >= 6.5) ? "Khá" :
             (avgScore < 6.5 && avgScore >= 5) ? "Trung Bình" :
