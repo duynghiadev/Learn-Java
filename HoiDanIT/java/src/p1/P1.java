8d367353fe6ae69409eb1a94e322df96a9f7c745
duynghiadev <duynghia22302@gmail.com>
2024-01-08 18:00:12 +0700

    #27. Access Modifier (private/public/protected) | Khóa Học Java Cơ Bản Từ A tới Z cho Beginner

---
+++ HoiDanIT/java/src/p1/P1.java
@@ -0,0 +1,11 @@
+package p1;
+
+public class P1 {
+  public static void main(String[] args) {
+    P firstObj = new P();
+    firstObj.a = 10;
+    firstObj.b = 10;
+    firstObj.c = 10;
+//    firstObj.d = 10;
+  }
+}
