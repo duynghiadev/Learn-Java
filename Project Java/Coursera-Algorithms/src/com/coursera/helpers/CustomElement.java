11be16147b5ab218e28090d0be86ff2baa291b30
duynghiadev <duynghia22302@gmail.com>
2024-10-30 22:15:15 +0700

    format code

--- Project Java/Coursera-Algorithms/src/com/coursera/helpers/CustomElement.java
+++ Project Java/Coursera-Algorithms/src/com/coursera/helpers/CustomElement.java
@@ -2,19 +2,19 @@ package com.coursera.helpers;
 
 public class CustomElement {
 
-	private long key;
-	private boolean value;
-	
-	public CustomElement(long key, boolean value) {
-		this.key = key;
-		this.value = value;
-	}
-	
-	public long getKey() {
-		return this.key;
-	}
-	
-	public boolean getValue() {
-		return this.value;
-	}
+    private long key;
+    private boolean value;
+
+    public CustomElement(long key, boolean value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public long getKey() {
+        return this.key;
+    }
+
+    public boolean getValue() {
+        return this.value;
+    }
 }
